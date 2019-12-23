36f546717b256083278a8cbe4feba06ba4fc82e3
Ciro Cirne Trindade <ciroct@gmail.com>
2019-12-23 20:08:43 -0300

    Cliente, TipoCliente, telefones e endereços

--- src/main/java/com/ciro/cursomc/CursomcApplication.java
+++ src/main/java/com/ciro/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ciro.cursomc.domain.Categoria;
 import com.ciro.cursomc.domain.Cidade;
+import com.ciro.cursomc.domain.Cliente;
+import com.ciro.cursomc.domain.Endereco;
 import com.ciro.cursomc.domain.Estado;
 import com.ciro.cursomc.domain.Produto;
+import com.ciro.cursomc.domain.enums.TipoCliente;
 import com.ciro.cursomc.repositories.CategoriaRepository;
 import com.ciro.cursomc.repositories.CidadeRepository;
+import com.ciro.cursomc.repositories.ClienteRepository;
+import com.ciro.cursomc.repositories.EnderecoRepository;
 import com.ciro.cursomc.repositories.EstadoRepository;
 import com.ciro.cursomc.repositories.ProdutoRepository;
 
@@ -27,6 +32,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private EstadoRepository estadoRepo;
 	@Autowired
 	private CidadeRepository cidadeRepo;
+	@Autowired
+	private ClienteRepository clienteRepo;
+	@Autowired
+	private EnderecoRepository enderecoRepo;
 	
 	
 	public static void main(String[] args) {
@@ -64,6 +73,18 @@ public class CursomcApplication implements CommandLineRunner{
 
 		estadoRepo.saveAll(Arrays.asList(uf1, uf2));
 		cidadeRepo.saveAll(Arrays.asList(c1, c2, c3));
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "434985445377", TipoCliente.PESSOAFISICA);
+		
+		Endereco e1 = new Endereco(null, "Rua das Flores", "300", "Sala 800", "Centro", "38777012", cli1, c1);
+		Endereco e2 = new Endereco(null, "Av. Mattos", "105", "Ap. 209", "Jardins", "38778912", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e2, e2));
+		cli1.getTelefones().addAll(Arrays.asList("23488348", "983238383"));
+		
+		clienteRepo.saveAll(Arrays.asList(cli1));
+		enderecoRepo.saveAll(Arrays.asList(e1, e2));
+		
 	}
 
 }
---
+++ src/main/java/com/ciro/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.ciro.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ciro.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
