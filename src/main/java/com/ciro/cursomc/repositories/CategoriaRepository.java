aa93b4e7872e403d015ad546ebf32a9fc898b24e
Ciro Cirne Trindade <ciroct@gmail.com>
2019-12-23 15:12:14 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/ciro/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.ciro.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ciro.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
